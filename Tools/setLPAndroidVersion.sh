b14a63bb5ac9274cf3cf6d68b7d43de080e2830f
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2020-04-20 13:01:18 -0700

    set library versions through env variables

---
+++ Tools/setLPAndroidVersion.sh
@@ -0,0 +1,1 @@
+if [ -z "$LEANPLUM_ANDROID_SDK_VERSION" ]; then version="latest"; else sed -i -e "s/implementation 'com.leanplum:leanplum-core:.*'/implementation 'com.leanplum:leanplum-core:$LEANPLUM_ANDROID_SDK_VERSION'/g" "android/build.gradle";rm android/build.gradle-e; fi
---
+++ Tools/setLPiOSVersion.sh
@@ -0,0 +1,1 @@
+if [ -z "$LEANPLUM_IOS_SDK_VERSION" ]; then version="latest"; else sed -i '' -e "s/s.dependency 'Leanplum-iOS-SDK', '.*'/s.dependency 'Leanplum-iOS-SDK', '$LEANPLUM_IOS_SDK_VERSION'/g" "react-native-leanplum.podspec"; fi
