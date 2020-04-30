63ab9bfcd36c068d516f802a511b904c82ddaea7
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2020-04-29 20:56:23 -0700

    create tag when triggered with env variables

---
+++ Tools/createTag.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function incrementBetaNumber() {
+	source Tools/semver.sh  
+
+	local MAJOR=0
+	local MINOR=0
+	local PATCH=0
+	local SPECIAL=""
+
+	semverParseInto `cat sdk-version.txt` MAJOR MINOR PATCH SPECIAL
+	if [ "$SPECIAL" == "" ]
+	then
+		PATCH=$((PATCH+1))
+		new_version="$MAJOR.$MINOR.$PATCH-beta1"
+	else
+		string=$SPECIAL
+		prefix="-beta"
+		beta_num=${string#"$prefix"}
+		beta_num=$((beta_num+1))
+		new_version="$MAJOR.$MINOR.$PATCH-beta$beta_num"
+	fi
+	echo $new_version > sdk-version.txt	
+}
+
+if [ -z "$LEANPLUM_ANDROID_SDK_VERSION" ] && [ -z "$LEANPLUM_IOS_SDK_VERSION" ]
+then
+	echo "skipping tagging"
+else
+	# bump sdk-version to beta
+	git add android/build.gradle
+	git add react-native-leanplum.podspec
+	incrementBetaNumber
+	git commit -m "Bump wrapped SDK Version"
+	git tag `cat sdk-version.txt`
+	git push
+	git push origin `cat sdk-version.txt`
+fi
--- Tools/setLPAndroidVersion.sh
+++ Tools/setLPAndroidVersion.sh
@@ -1,1 +1,8 @@
-if [ -z "$LEANPLUM_ANDROID_SDK_VERSION" ]; then version="latest"; else sed -i -e "s/implementation 'com.leanplum:leanplum-core:.*'/implementation 'com.leanplum:leanplum-core:$LEANPLUM_ANDROID_SDK_VERSION'/g" "android/build.gradle";rm android/build.gradle-e; fi
+#!/bin/bash
+if [ -z "$LEANPLUM_ANDROID_SDK_VERSION" ]
+then 
+	version="latest"
+else 
+	sed -i -e "s/implementation 'com.leanplum:leanplum-core:.*'/implementation 'com.leanplum:leanplum-core:$LEANPLUM_ANDROID_SDK_VERSION'/g" "android/build.gradle"
+	rm android/build.gradle-e
+fi
