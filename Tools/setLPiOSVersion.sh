b449d6966a577d5d2ceb0830d3d7b37ed63b23df
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2020-04-29 21:04:42 -0700

    fix build script for linux

--- Tools/setLPiOSVersion.sh
+++ Tools/setLPiOSVersion.sh
@@ -3,5 +3,4 @@ then
 	version="latest"
 else 
 	sed -i -e "s/s.dependency 'Leanplum-iOS-SDK', '.*'/s.dependency 'Leanplum-iOS-SDK', '$LEANPLUM_IOS_SDK_VERSION'/g" "react-native-leanplum.podspec"
-	rm react-native-leanplum.podspec-e
 fi
