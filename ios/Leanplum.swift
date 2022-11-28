1f15b1db2931331883db9827ef3f4db7b05a5798
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2022-11-28 12:24:03 +0200

    Use MigrationManager config

--- ios/Leanplum.swift
+++ ios/Leanplum.swift
@@ -379,11 +379,10 @@ class RNLeanplum: RCTEventEmitter {
     func migrationConfig(_ resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) {
         var migrationConfig:[String: Any] = [:]
         migrationConfig["state"] = MigrationManager.shared.state.description
-        migrationConfig["accountId"] = UserDefaults.standard.string(forKey: "__leanplum_ct_account_key")
-        migrationConfig["accountToken"] = UserDefaults.standard.string(forKey: "__leanplum_ct_account_token")
-        migrationConfig["accountRegion"] = UserDefaults.standard.string(forKey: "__leanplum_region_code")
-        migrationConfig["attributeMappings"] = UserDefaults.standard.object(forKey: "__leanplum_attribute_mappings")
-         as? [String: String]
+        migrationConfig["accountId"] = MigrationManager.shared.cleverTapAccountId
+        migrationConfig["accountToken"] = MigrationManager.shared.cleverTapAccountToken
+        migrationConfig["accountRegion"] = MigrationManager.shared.cleverTapAccountRegion
+        migrationConfig["attributeMappings"] = MigrationManager.shared.cleverTapAttributeMappings
         resolve(migrationConfig)
     }
 }
